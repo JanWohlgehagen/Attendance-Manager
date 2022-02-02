f11ecca2bbed71160a1bed353cb163573c4f48fb
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2022-02-02 10:08:56 +0100

    Project remade

---
+++ src/gui/App.java
@@ -0,0 +1,21 @@
+package gui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("Views/View.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 680, 400);
+        primaryStage.setTitle("Attendance Manager");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
