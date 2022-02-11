3d4a38001393f614a7cfc9013eff0a7b5ef5d31b
Tobias Rasnussen <tobi9782@easv365.dk>
2022-02-11 14:31:33 +0100

    add

--- src/gui/Controllers/TeacherMenuController.java
+++ src/gui/Controllers/TeacherMenuController.java
@@ -2,13 +2,16 @@ package gui.Controllers;
 
 import be.Student;
 import gui.Models.StudentListModel;
-import gui.Models.TeacherListModel;
 import gui.util.SceneSwapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -39,6 +42,7 @@ public class TeacherMenuController implements Initializable {
     private ObservableList<Student> viewStudent;
     private SceneSwapper sceneSwapper;
 
+
     public TeacherMenuController() {
         sceneSwapper = new SceneSwapper();
         studentListModel = new StudentListModel();
@@ -54,6 +58,30 @@ public class TeacherMenuController implements Initializable {
         tcAbsence.setCellValueFactory(student -> student.getValue().getAbsentInPercent());
 
         cbClass.getItems().addAll("Dansk");
+
+
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Most Popular Programming Language");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("# of developers x 1000");
+
+        this.barChart = new BarChart(xAxis, yAxis);
+
+
+        XYChart.Series dataSeries1 = new XYChart.Series();
+        dataSeries1.setName("Popular programming languages rated by GitHub");
+
+        dataSeries1.getData().add(new XYChart.Data("JavaScript", 2300));
+        dataSeries1.getData().add(new XYChart.Data("Python", 1000));
+        dataSeries1.getData().add(new XYChart.Data("Java", 986));
+        dataSeries1.getData().add(new XYChart.Data("Ruby", 870));
+        dataSeries1.getData().add(new XYChart.Data("C++", 413));
+        dataSeries1.getData().add(new XYChart.Data("C#", 326));
+        barChart.getData().add(dataSeries1);
+
+
+
 
     }
 
@@ -75,5 +103,33 @@ public class TeacherMenuController implements Initializable {
         lalGPName.setText(tvStudent.getSelectionModel().getSelectedItem().getfullName().getValue());
         lalGPStudentID.setText(tvStudent.getSelectionModel().getSelectedItem().getStudentID().getValue());
         lalGPAbsence.setText(tvStudent.getSelectionModel().getSelectedItem().getAbsentInPercent().getValue());
+    }
+
+    public void handleLogOut(ActionEvent actionEvent) {
+
+    }
+
+    private BarChart buildBarChart() {
+        CategoryAxis xAxis = new CategoryAxis();
+        xAxis.setLabel("Most Popular Programming Language");
+
+        NumberAxis yAxis = new NumberAxis();
+        yAxis.setLabel("# of developers x 1000");
+
+        this.barChart = new BarChart(xAxis, yAxis);
+
+
+        XYChart.Series dataSeries1 = new XYChart.Series();
+        dataSeries1.setName("Popular programming languages rated by GitHub");
+
+        dataSeries1.getData().add(new XYChart.Data("JavaScript", 2300));
+        dataSeries1.getData().add(new XYChart.Data("Python", 1000));
+        dataSeries1.getData().add(new XYChart.Data("Java", 986));
+        dataSeries1.getData().add(new XYChart.Data("Ruby", 870));
+        dataSeries1.getData().add(new XYChart.Data("C++", 413));
+        dataSeries1.getData().add(new XYChart.Data("C#", 326));
+        barChart.getData().add(dataSeries1);
+
+        return barChart;
     }
 }
